247d268ee28050c87b2f3459ddaf26b7b85c26c7
John Donaghy <jdonaghy@oldrepublictitle.com>
2025-09-04 11:02:25 -0500

    Fix keymaps and loading issues

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -49,29 +49,3 @@ vim.keymap.set('n', '<leader>dd', ':lua require("dap").continue()<CR>')
 vim.keymap.set('n', '<leader>do', ':lua require("dap").step_over()<CR>')
 vim.keymap.set('n', '<leader>di', ':lua require("dap").step_ito()<CR>')
 
--- Telescope keymaps
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
-
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sb', require("telescope.builtin").buffers, { desc = '[S]earch [B]uffers ' })
-vim.keymap.set('n', '<leader>sc', require("telescope.builtin").commands, { desc = '[S]earch [C]ommands ' })
-vim.keymap.set('n', '<leader>st', require("telescope.builtin").treesitter, { desc = '[S]earch [T]reesitter ' })
-vim.keymap.set('n', '<leader>ss', require("telescope.builtin").current_buffer_fuzzy_find, { desc = '[S]earch [s]search ' })
-vim.keymap.set('n', '<leader>sr', require("telescope").extensions.live_grep_args.live_grep_args, { noremap = true, desc = '[S]earch current [R]ipgrep ' })
-
--- UFO (code folding) keymaps
-vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
