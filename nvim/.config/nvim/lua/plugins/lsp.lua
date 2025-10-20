5ea687e5cfc99025c143bdce02f2a7439e811a3e
John Donaghy <jdonaghy@oldrepublictitle.com>
2025-10-20 09:33:47 -0500

    improve c# experience, rm barbar and old completion plugin

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,15 +1,15 @@
 return {
-  { 'Hoffs/omnisharp-extended-lsp.nvim' },
-  { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
+  { 'VonHeikemen/lsp-zero.nvim',    branch = 'v3.x' },
   { 'Issafalcon/lsp-overloads.nvim' },
-  { 'mason-org/mason.nvim',
+  {
+    'mason-org/mason.nvim',
     config = function()
       require("mason").setup({
         registries = {
-            "github:mason-org/mason-registry",
-            "github:Crashdummyy/mason-registry",
+          "github:mason-org/mason-registry",
+          "github:Crashdummyy/mason-registry",
         },
-    })
+      })
     end
   },
   {
@@ -29,7 +29,8 @@ return {
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      { 'mason-org/mason-lspconfig.nvim',
+      {
+        'mason-org/mason-lspconfig.nvim',
         dependencies = { 'neovim/nvim-lspconfig' },
         opts = {}
       },
--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -70,9 +70,9 @@ return {
           highlight_git = true, -- Highlights files/folders with git status colors
           icons = {
             show = {
-              git = true,        -- Shows git status icons (A/M/D/untracked, etc.)
-              folder = true,     -- Shows folder icons
-              file = true,       -- Shows file icons
+              git = true,          -- Shows git status icons (A/M/D/untracked, etc.)
+              folder = true,       -- Shows folder icons
+              file = true,         -- Shows file icons
               folder_arrow = true, -- Shows arrows for expandable folders
             },
           },
@@ -91,62 +91,14 @@ return {
       vim.api.nvim_set_keymap('n', '<Space>bv', '<Cmd>ShowBufferList<CR>', { noremap = true, silent = true })
     end,
   },
-
   {
-    'romgrk/barbar.nvim',
-    dependencies = {
-      'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
-      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-    },
-    init = function()
-      vim.g.barbar_auto_setup = false
-      local map = vim.api.nvim_set_keymap
-      local opts = { noremap = true, silent = true }
-
-      -- Move to previous/next
-      map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-      map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
-      -- Re-order to previous/next
-      map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-      map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
-      -- Goto buffer in position...
-      map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
-      map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
-      map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
-      map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
-      map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
-      map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
-      map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
-      map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
-      map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
-      map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
-      -- Pin/unpin buffer
-      map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
-      -- Close buffer
-      map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
-      -- Wipeout buffer
-      --                 :BufferWipeout
-      -- Close commands
-      --                 :BufferCloseAllButCurrent
-      --                 :BufferCloseAllButPinned
-      --                 :BufferCloseAllButCurrentOrPinned
-      --                 :BufferCloseBuffersLeft
-      --                 :BufferCloseBuffersRight
-      -- Magic buffer-picking mode
-      map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
-      -- Sort automatically by...
-      map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
-      map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
-      map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
-      map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require 'colorizer'.setup({
+        '*',                -- Highlight all files, but customize some others.
+        css = { rgb_fn = true, }, -- Enable parsing rgb(...) functions in css.
+        html = { names = false, } -- Disable parsing "names" like Blue or Gray
+      })
     end,
-    opts = {
-      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-      -- animation = true,
-      -- insert_at_start = true,
-      -- …etc.
-    },
-    version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
 }
-
